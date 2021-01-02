021560b5f16f5cb28b25d176b307deff3c68c3e1
11cconnolly <callumc11@gmail.com>
2021-01-02 22:44:15 +0000

    First Day both challenges completed

--- AoC/Day1/Day1.cs
+++ AoC/Day1/Day1.cs
@@ -1,10 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
-namespace AoC.Day1
+namespace AoC
 {
     class Day1
     {
+        public static void Main(string[] args)
+        {
+            // Will need to change this path depending on where the program is running
+            string path = @"C:\Users\cconnolly\source\repos\AoC\AoC\Day1\input.txt";
+
+            List<int> values = ParseHelper(path);
+
+            int result;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // result = sumTwoTo2020(values);
+            result = sumThreeTo2020(values);
+
+            sw.Stop();
+
+            if (result < 0)
+                Console.WriteLine("[Error] Error in calculation");
+
+            Console.WriteLine("result is {0} in {1}ms", result, sw.ElapsedTicks);
+        }
+
+        // Answer is 969024
+        private static int sumTwoTo2020
+            (List<int> values)
+        {
+            int a, b;
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                a = values[i];
+                for (int j = 0; j < values.Count; j++)
+                {
+                    b = values[j];
+                    if (a + b == 2020)
+                        return a * b;
+                }
+            }
+
+            return -1;
+        }
+
+        // Answer is 230057040
+        private static int sumThreeTo2020
+            (List<int> values)
+        {
+            int a, b, c;
+
+            for (int i = 0; i < values.Count - 2; i++)
+            {
+                a = values[i];
+                for (int j = i + 1; j < values.Count - 1; j++)
+                {
+                    b = values[j];
+                    for (int k = j + 1; k < values.Count; k++)
+                    {
+                        c = values[k];
+                        if (a + b + c == 2020)
+                            return a * b * c;
+                    }
+
+                }
+                values.RemoveAt(i);
+            }
+
+            return -1;
+        }
+
+        private static List<int> ParseHelper (string path)
+        {
+            string line;
+            List<int> values = new List<int>();
+
+            System.IO.StreamReader reader = new System.IO.StreamReader(path);
+            while ((line = reader.ReadLine()) != null)
+            {
+                values.Add(int.Parse(line));
+            }
+
+            return values;
+        }
+
     }
 }
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -6,7 +6,7 @@ namespace AoC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
