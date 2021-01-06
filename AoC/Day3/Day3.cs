f9cd12d5c96a73d904cfb546d12bcf95992fab59
11cconnolly <callumc11@gmail.com>
2021-01-06 20:04:26 +0000

    Progress towards Day 3 Part 2 but not quite there

--- AoC/Day3/Day3.cs
+++ AoC/Day3/Day3.cs
@@ -9,13 +9,14 @@ namespace AoC
     {
         public static void Main(string[] args)
         {
-            runPart1();
+            Console.WriteLine("part two");
+            RunPart1();
+            Console.WriteLine("part two");
+            RunPart2();
         }
 
-        public static void runPart1()
+        public static void RunPart1()
         {
-            List<string> lines = new List<string>();
-
             string line;
             StreamReader reader = new StreamReader(@"C:\Users\cconnolly\source\repos\AoC\AoC\Day3\input.txt");
 
@@ -25,17 +26,70 @@ namespace AoC
             reader.ReadLine();
             while ((line = reader.ReadLine()) != null)
             {
-                if (countRight < 28)
-                    countRight += 3;
+                if (countRight < 30)
+                    countRight += 1;
                 else
-                    countRight -= 28;
+                    countRight -= 30;
 
                 if (line[countRight].Equals('#'))
                     numberOfTrees++;
-
             }
 
             Console.WriteLine(numberOfTrees);
+        }
+        public static void RunPart2()
+        {
+            int LINE_LENGTH = 31;
+            int[] rightMovesArray = {1, 3, 5, 7};
+            long sumOfNumberOfTrees = 1;
+
+            for (int i = 0; i < rightMovesArray.Length; i++)
+            {
+                string line;
+                StreamReader reader = new StreamReader(@"C:\Users\cconnolly\source\repos\AoC\AoC\Day3\input.txt");
+
+                int countRight = 0;
+                int numberOfTrees = 0;
+
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (countRight < LINE_LENGTH - rightMovesArray[i])
+                        countRight += rightMovesArray[i];
+                    else
+                        countRight -= (LINE_LENGTH - rightMovesArray[i]);
+
+                    if (line[countRight].Equals('#'))
+                        numberOfTrees++;
+                }
+                Console.WriteLine(numberOfTrees);
+            }
+
+            rightMovesArray = new int[] { 1 };
+            for (int i = 0; i < rightMovesArray.Length; i++)
+            {
+                string line;
+                StreamReader reader = new StreamReader(@"C:\Users\cconnolly\source\repos\AoC\AoC\Day3\input.txt");
+
+                int countRight = 0;
+                int numberOfTrees = 0;
+
+                reader.ReadLine(); // 0
+                while ((line = reader.ReadLine()) != null) // 1
+                {
+                    reader.ReadLine(); // 2
+                    if (countRight < LINE_LENGTH - rightMovesArray[i])
+                        countRight += rightMovesArray[i];
+                    else
+                        countRight -= (LINE_LENGTH - rightMovesArray[i]);
+
+                    if (line[countRight].Equals('#'))
+                        numberOfTrees++;
+                }
+                Console.WriteLine(numberOfTrees);
+            }
+
+            Console.WriteLine(sumOfNumberOfTrees);
         }
     }
 }
